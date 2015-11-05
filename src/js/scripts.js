3d73b9988743daf213e34438cd3b315be964fbc4
Adam Winick <sinelanguage@gmail.com>
2015-11-05 09:24:56 -0500

    work in progress on lies.html JS animation

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,31 +5,51 @@
 	$( function () {
 		// FireShell
 
-                var lieArr = [
-                        "I didn\'t want to hurt her"
+		var lieArr = [
+                        "I didn\'t want to hurt her",
+                        "I didn\'t want them to know how I really feel",
+                        "I\'m a corward",
+                        "I\'m in denial of how addicted I am",
+                        "I\'m scared",
+                        "I really don\'t like you",
+                        "They will think I\'m stupid",
+                        "I don\'t want be seen as weak"
                 ];
 
-                var el = document.getElementsByClassName( 'reason' );
-                el[0].innerText = lieArr[0];
+		var el = document.getElementsByClassName( 'reason' );
+		el[ 0 ].innerText = lieArr[ 0 ];
 
-		function addClass( elClassName, className ) {
+		function addClass( el, className ) {
 
-			var el = document.getElementsByClassName( elClassName );
 			el[ 0 ].classList.add( className );
 			setTimeout( function () {
-				el[ 0 ].remove()
+				el[ 0 ].innerText = "";
+                                removeClass( className );
 			}, 2000 );
 
-		}
+		};
 
-		function removeClass( className ) {
+		function removeClass( el, className ) {
 
 			var el = document.getElementsByClassName( className );
 			el[ 0 ].classList.remove( className );
 
-		}
+		};
 
-		setTimeout( function(){ addClass( 'reason', 'fade-out' ) }, 5000 );
+		// setTimeout( function(){ addClass( 'reason', 'fade-out' ) }, 5000 );
+
+		function runThruLies() {
+
+                        var el = document.getElementsByClassName( 'reason' );
+
+			for ( var i = 0, i < lieArr.length, i++ ) {
+
+                		el[ 0 ].innerText = lieArr[ i ];
+                                setTimeout( function(){ addClass( 'reason', 'fade-out' ) }, 5000 );
+
+			}
+
+		};
 
 	} );
 
